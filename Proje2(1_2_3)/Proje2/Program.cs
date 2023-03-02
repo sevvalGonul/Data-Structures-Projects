d39813f8d1cf1e955595d620e61f0a1733dc5438
Şevval Gönül <sevvalgnl17@gmail.com>
2023-03-02 16:54:02 +0300

    List, Stack, Queue, PQ

---
+++ Proje2(1_2_3)/Proje2/Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Proje2
+{
+    class Teslimat
+    {
+        public string yemekAdi;
+        public int adet;
+
+        public Teslimat(string yemekAdi, int adet)
+        {
+            this.yemekAdi = yemekAdi;
+            this.adet = adet;
+        }
+    }
+
+    class Mahalle
+    {
+        public string mahalleAdi;
+        public List<Teslimat> teslimatlar;
+
+        public Mahalle(string mahalleAdi)
+        {
+            this.mahalleAdi = mahalleAdi;
+            teslimatlar = new List<Teslimat>();
+        }
+        public override string ToString()
+        {
+            string str = "Mahalle Adı: " + mahalleAdi + ", Teslimatlar: ";
+            foreach(Teslimat teslimat in teslimatlar)
+            {
+                str += String.Format("[{0}, {1}] ", teslimat.yemekAdi, teslimat.adet);
+            }
+            return str;
+        }
+    }
+
+    class Stack
+    {
+        private int maxSize;
+        private Mahalle[] stackArray;  // Mahalle tipindeki nesneleri tutacak olan Stack
+        private int top;
+
+        public Stack(int size)
+        {
+            maxSize = size;
+            stackArray = new Mahalle[maxSize];
+            top = -1;
+        }
+
+        public void push(Mahalle newItem)
+        {
+            stackArray[++top] = newItem;
+        }
+
+        public Mahalle pop()
+        {
+            return stackArray[top--];
+        }
+
+        public bool isEmpty()
+        {
+            return (top == -1);
+        }
+    }
+
+    class Queue
+    {
+        private int maxSize;
+        private Mahalle[] queueArray;  // Mahalle tipindeki nesneleri tutacak olan Queue
+        private int front; private int rear; private int nItems;
+
+        public Queue(int size)
+        {
+            maxSize = size;
+            queueArray = new Mahalle[maxSize];
+            front = 0;
+            rear = -1;
+            nItems = 0;
+        }
+
+        public void insert(Mahalle newItem)  // Kuyruğa eleman ekleme
+        {
+            if (rear == maxSize - 1)
+                rear = -1;
+            queueArray[++rear] = newItem;
+            nItems++;
+        }
+
+        public Mahalle remove()
+        {
+            Mahalle temp = queueArray[front++];
+            if (front == maxSize)
+                front = 0;
+            nItems--;
+            return temp;
+        }
+
+        public bool isEmpty()
+        {
+            return (nItems == 0);
+        }
+    }
+
+    class OncelikliKuyruk
+    {
+        List<Mahalle> pq;  // Priority Queue
+
+        public OncelikliKuyruk()
+        {
+            pq = new List<Mahalle>();
+        }
+
+        public void ekle(Mahalle yeniMahalle)  // Eleman kuyruğun sonuna eklenir
+        {
+            pq.Add(yeniMahalle);  // Add metodu yeni gelen elemanı sona ekler
+        }
+
+        public Mahalle sil()  // Azalan Öncelik Kuyruğu olduğu için önce en fazla teslimat yapılan mahalleyi silecek olan metod
+        {
+            int maxTeslimatSay = 0;  // Initialize etmek için max değişkenine olamayacak kadar küçük bir değer veriyorum
+            Mahalle maxTeslimatliMahalle = pq[0];  // Silinecek olan elemanı kuyruktaki ilk eleman olarak belirledim. Döngüde güncellenecek.
+
+            foreach (Mahalle mahalle in pq)
+            {
+                int teslimatSay = mahalle.teslimatlar.Count;  // O mahalledeki teslimat sayısı
+                if (teslimatSay > maxTeslimatSay)
+                {
+                    maxTeslimatSay = teslimatSay;
+                    maxTeslimatliMahalle = mahalle;
+                }
+            }
+            // Döngüden çıkınca silinecek olan eleman yani en fazla teslimat yapılacak olan mahalle bulunmuş olur.
+            pq.Remove(maxTeslimatliMahalle);  // List classının Remove metoduna argüman olarak silinecek olan eleman verilir ve o eleman listeden kaldırılmış olur
+            return maxTeslimatliMahalle;  // Silinen eleman geri döndürülür
+        }
+
+        public bool bosMu()
+        {
+            return (pq.Count == 0);
+        }
+    }
+    class Program
+    {
+        static string[] yemekListesi = { "İçli Köfte", "Pizza", "Hamburger", "Mantı", "Döner", "Börek", "Pilav", "Türlü", "Simit", "Kızartma" };
+        static Random random = new Random();
+        static ArrayList bilesikVeriYapisiOlustur(string[] mahalleAdlari, int[] teslimatSayisi)
+        {
+            ArrayList motoKurye = new ArrayList();
+            for(int i = 0; i < mahalleAdlari.Length; i++)  // Herbir mahalle için:
+            {
+                string mahalleAdi = mahalleAdlari[i];
+                int teslimatSay = teslimatSayisi[i];
+                Mahalle mahalle = new Mahalle(mahalleAdi);  // Yeni bir mahalle ve o mahalledeki teslimatları tutmak için bir GenericList oluşturdu.
+                
+                for(int j = 0; j < teslimatSay; j++)  // O mahalledeki herbir teslimat için:
+                {
+                    int rastgeleSayi = random.Next(0, yemekListesi.Length); // 0 ile yemek listesinin uzunluğu arasında rastgele bir sayi
+                    Teslimat teslimat = new Teslimat(yemekListesi[rastgeleSayi], random.Next(1,15));  // yemekListesinde rastgeleSayiya karşılık gelen yemek ile 1-15 arasında bir adette oluşturulan teslimat
+                    mahalle.teslimatlar.Add(teslimat);  // mahalle nesnesinin içindeki teslimatlar GenericList'ine teslimat nesnesi ekleme
+                }
+
+                motoKurye.Add(mahalle);
+            }
+            return motoKurye;
+        }
+
+        static void yazdir(ArrayList motoKurye)
+        {
+            int teslimatSay = 0;  // Toplam teslimat sayısı
+            foreach (Mahalle mahalle in motoKurye)
+            {
+                Console.Write("Mahalle Adı: " + mahalle.mahalleAdi + ", ");
+                Console.Write("Teslimatlar: ");
+
+                foreach (Teslimat teslimat in mahalle.teslimatlar)
+                {
+                    Console.Write("[{0}, {1}] ", teslimat.yemekAdi, teslimat.adet);
+                    teslimatSay += 1;
+                }
+                Console.WriteLine();
+            }
+
+            // Toplam liste ve teslimat sayısı yazdırma:
+            Console.WriteLine("\nArrayList içindeki GenericList Sayısı (Mahalle Sayısı) : " + motoKurye.Count);
+            Console.WriteLine("Toplam teslimat sayısı: " + teslimatSay + "\n");
+        }
+        static void Main(string[] args)
+        {
+            string[] mahalleAdlari = { "Özkanlar", "Evka 3", "Atatürk", "Erzene", "Kazımdirik", "Mevlana", "Doğanlar", "Ergene" };
+            int[] teslimatSayisi = { 5, 2, 7, 2, 7, 3, 0, 1};
+
+            ArrayList motoKurye = bilesikVeriYapisiOlustur(mahalleAdlari, teslimatSayisi);
+            yazdir(motoKurye);
+
+
+            // Yığıt, kuyruk ve öncelikli kuyruk oluşturma ve tüm mahalleleri ArrayList'ten çekerek bu veri yapılarına eklema:
+            Stack stack = new Stack(mahalleAdlari.Length);
+            Queue queue = new Queue(mahalleAdlari.Length);
+            OncelikliKuyruk priorityQueue = new OncelikliKuyruk();
+            
+            foreach (Mahalle mahalle in motoKurye)
+            {
+                stack.push(mahalle);
+                queue.insert(mahalle);
+                priorityQueue.ekle(mahalle);
+            }
+
+            // Yığıttaki elemanları ekrana yazdırma:
+            Console.WriteLine("Yığıt:");
+            while(!stack.isEmpty())
+            {
+                Console.WriteLine(stack.pop().ToString());
+            }
+
+            Console.WriteLine("\nKuyruk: ");
+            // Kuyruktaki elemanları ekrana yazdırma:
+            while(!queue.isEmpty())
+                Console.WriteLine(queue.remove().ToString());
+
+            Console.WriteLine("\nÖncelikli Kuyruk: ");
+            // Öncelikli Kuyruktaki elemanları ekrana yazdırma:
+            while (!priorityQueue.bosMu())
+                Console.WriteLine(priorityQueue.sil().ToString());
+        }
+    }
+}
---
+++ Project2_4/Project2(4)/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project2_4_
+{
+    class Kuyruk
+    {
+        List<int> queueList;
+
+        public Kuyruk()
+        {
+            queueList = new List<int>();
+        }
+
+        public void ekle(int yeniEleman)  // Kuyruk yapısında elemanlar sondan eklenir
+        {
+            queueList.Add(yeniEleman);  // Add metodu sondan ekleme yapar
+        }
+
+        public int sil()  // Kuyruk yapısında elemanlar baştan silinir.
+        {
+            int silinen = queueList[0];
+            queueList.RemoveAt(0);  // En baştaki (0.indexteki) elemanı silme
+            return silinen;
+        }
+
+        public bool bosMu()
+        {
+            return (queueList.Count == 0);
+        }
+
+    }
+
+    class OncelikliKuyruk
+    {
+        List<int> pq;
+
+        public OncelikliKuyruk()
+        {
+            pq = new List<int>();
+        }
+
+        public void ekle(int yeniEleman)
+        {
+            pq.Add(yeniEleman);  // Gelen eleman kuyruğun sonuna eklenir
+        }
+
+        public int oncelikliSil()  // Artan sırada öncelik kuyruğu olduğu için önce en küçük olan elemanı silecek olan metod
+        {
+            int min = pq[0];  // Öncelik kuyruğunun ilk elemanını minimum olarak initialize ettim, döngüde güncellenecek.
+            foreach(int sayi in pq)
+            {
+                if (sayi < min)
+                    min = sayi;
+            }
+            // Döngüden çıkınca en küçük eleman bulunmuş olur ve remove metoduyla o elemanı listeden kaldırıyorum:
+            pq.Remove(min);
+            return min;  // Öncelikli kuyrukta en az sayıda ürünü olan müşteri silinir ve ürünlerinin sayısı döndürülür.
+        }
+
+        public bool bosMu()
+        {
+            return (pq.Count == 0);
+        }
+    }
+    class Program
+    {
+        static int okutmaSuresi = 3;  // Herbir ürünün okutulması 3 saniye sürmektedir.
+        static void Main(string[] args)
+        {
+            int[] urunSay = { 6, 7, 2, 1, 12, 5, 3, 7, 4, 2 };  // Kasada bekleyen herbir müşterinin ürün sayıları
+            int musteriSay = urunSay.Length;
+
+            Kuyruk kuyruk = new Kuyruk();
+            OncelikliKuyruk pq = new OncelikliKuyruk();
+
+            for (int i = 0; i < musteriSay; i ++)  // Herbir müşterinin ürün sayıları kuyruk yapılarına eklenir.
+            {  
+                kuyruk.ekle(urunSay[i]);
+                pq.ekle(urunSay[i]);
+            }
+
+            int[] kuyrukIslemSureleri = new int[musteriSay];  // Kuyruk yapısındaki herbir müsterinin işlem tamamlanma sürelerini tutacak olan dizi
+            int[] pqIslemSureleri = new int[musteriSay];  // Öncelikli Kuyruk yapısındaki herbir müsterinin işlem tamamlanma sürelerini tutacak olan dizi
+
+            for (int i = 0; i < musteriSay; i++)
+            {
+                if (i == 0)  // Bu kuyruklardaki ilk müştesi ise, işlem süresi kendi ürünlerini okutma süresine eşittir
+                { 
+                    kuyrukIslemSureleri[i] = kuyruk.sil() * okutmaSuresi;
+                    pqIslemSureleri[i] = pq.oncelikliSil() * okutmaSuresi;
+                }
+                else  // Diğer müşteriler için işlem tamamlanma süresi, kendinden önceki müşterinin işlem tamamlanma süresi ile kendisinin ürünlerini okutma süresi toplamına eşittir.
+                {
+                    kuyrukIslemSureleri[i] = kuyrukIslemSureleri[i - 1] + kuyruk.sil() * okutmaSuresi;
+                    pqIslemSureleri[i] = pqIslemSureleri[i - 1] + pq.oncelikliSil() * okutmaSuresi;
+                }
+            }
+
+            int kuyrukİslemToplam = 0;
+            Console.Write("Kuyruktaki herbir müşterinin işlem tamamlanma süreleri: ");
+            foreach (int sure in kuyrukIslemSureleri)
+            {
+                Console.Write(sure + " ");
+                kuyrukİslemToplam += sure;
+            }
+
+            Console.Write("\nÖncelikli Kuyruktaki herbir müşterinin işlem tamamlanma süreleri: ");
+            int pqİslemToplam = 0;
+            foreach (int sure in pqIslemSureleri)
+            {
+                Console.Write(sure + " ");
+                pqİslemToplam += sure;
+            }
+
+            Console.WriteLine("\nKuyruk yapısı kullanılan kasada ortalama işlem tamamlanma süresi: " + (double)kuyrukİslemToplam/musteriSay);
+            Console.WriteLine("Öncelikli Kuyruk yapısı kullanılan kasada ortalama işlem tamamlanma süresi: " + (double)pqİslemToplam/musteriSay);
+
+        }
+    }
+}
